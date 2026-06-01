4d71eadedab3bd2e3a7eb21c55037e176b187d3d
Jim Berlage <jamesberlage@gmail.com>
2026-06-01 09:39:45 -0500

    Add in SHA256

--- Formula/ahab.rb
+++ Formula/ahab.rb
@@ -7,18 +7,18 @@ class Ahab < Formula
   on_macos do
     on_arm do
       url "https://github.com/jimberlage/ahab/releases/download/v1.0.0/ahab-aarch64-apple-darwin.tar.gz"
-      sha256 "<CHANGE ME>"
+      sha256 "93368a3f7413d5f69968a2f58fe5a94b379fd262d2915f8beef6d8375a34fe48"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/jimberlage/ahab/releases/download/v1.0.0/ahab-aarch64-linux.tar.gz"
-      sha256 "<CHANGE ME>"
+      sha256 "658020e2afa200306b8dab66da17f9acc3833d813d5bf9b84fba271f53cdb97e"
     end
     on_intel do
       url "https://github.com/jimberlage/ahab/releases/download/v1.0.0/ahab-x86_64-linux.tar.gz"
-      sha256 "<CHANGE ME>"
+      sha256 "d2a0c488dce1bbc3d3ba17121f7e482ca0686c6011eac5ed42a108b6ca308f95"
     end
   end
 
