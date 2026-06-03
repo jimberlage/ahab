775480f45163156ccd3a3bca4456348756144ae6
Jim Berlage <jamesberlage@gmail.com>
2026-06-03 13:00:37 -0500

    Update with SHA256

--- Formula/ahab.rb
+++ Formula/ahab.rb
@@ -7,18 +7,18 @@ class Ahab < Formula
   on_macos do
     on_arm do
       url "https://github.com/jimberlage/ahab/releases/download/v4.0.0/ahab-aarch64-apple-darwin.tar.gz"
-      sha256 "<CHANGE ME>"
+      sha256 "6607440df8a81cf99197ee788dc8ae482f908b373080aa6fb9f6b2b586557d8d"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/jimberlage/ahab/releases/download/v4.0.0/ahab-aarch64-linux.tar.gz"
-      sha256 "<CHANGE ME>"
+      sha256 "503d206ffdbc2489b76deea62fac102f0c19102bb67b690cc5ac6d0295370309"
     end
     on_intel do
       url "https://github.com/jimberlage/ahab/releases/download/v4.0.0/ahab-x86_64-linux.tar.gz"
-      sha256 "<CHANGE ME>"
+      sha256 "326e64cd606bd34368c8f4caa31389139e07b3bff6f7f752ccb06920b896c0f1"
     end
   end
 
